9ff8f276c6b86570a4efabcf78f27d30ea05561e
kokiito <itou-kouki@aidemy.co.jp>
2023-05-28 22:35:28 +0900

    ref: move blog page

--- microcms-next-jamstack-blog/app/page.tsx
+++ microcms-next-jamstack-blog/app/page.tsx
@@ -1,33 +1,15 @@
 'use client';
-import Link from "next/link";
-import { Blog, getList } from "./libs/client";
+import { getList } from "./libs/client";
 import { Button  } from '@chakra-ui/react';
-import { use, useEffect, useState } from "react";
-import { MicroCMSListResponse } from "microcms-js-sdk/dist/cjs/types";
 
 // TODO: not Foundの場合の処理を追加する
 export default async function Home() {
 
-  const { contents } = await getList();
-
-  if (!contents || contents.length === 0) {
-  return <h1>No contents</h1>;
-  } 
-
   return (
     <div>
-      <Button colorScheme="teal" variant="outline" onClick={() => console.log({data2:contents})}>
+      <Button colorScheme="teal" variant="outline"> 
         ボタン
       </Button>
-      <ul>
-        {contents.map((post) => {
-          return (
-            <li key={post.id}>
-              <Link href={`/blogs/${post.id}`}>{post.title}</Link>
-            </li>
-          );
-        })}
-      </ul>
     </div>
   );
 }
