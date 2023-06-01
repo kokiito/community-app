f02f7ff2c8a9ca70052319a54f5c04c6ef341557
kokiito <itou-kouki@aidemy.co.jp>
2023-06-01 22:42:09 +0900

    feat: make origin page design move

---
+++ app/components/Hero.tsx
@@ -0,0 +1,44 @@
+import { Box, Flex, Center, Heading, Button, Image } from "@chakra-ui/react";
+import React from "react";
+import localFont from "next/font/local";
+
+// Font files can be colocated inside of `pages`
+const myFont = localFont({ src: "../851H-kktt_004.ttf" });
+
+export const Hero = () => {
+  return (
+    <Box w="100%" h="100vh" className={myFont.className}>
+      <Image
+        src={"/headerIcon.png"}
+        position={"absolute"}
+        top={"24px"}
+        left={"32px"}
+        width={"70px"}
+        zIndex={"docked"}
+      />
+      <Flex justifyContent={"center"} height={"100%"}>
+        <Center w="400px">
+          <Box zIndex={"docked"}>
+            <Box rounded="lg" bg={"white"} p={"16px"}>
+              <Heading className={myFont.className}>しゃるうぃー</Heading>
+              <Heading className={myFont.className}>クライミング</Heading>
+            </Box>
+            <Center mt={"32px"}>
+              <Button as="a" href={"https://tunagate.com/circle/81865"}>
+                つなげーとで募集中
+              </Button>
+            </Center>
+          </Box>
+        </Center>
+        <Box
+          flex="1"
+          backgroundImage={"/borudaringukabe.jpg"}
+          backgroundSize={"cover"}
+          position={["absolute", "absolute", "initial"]}
+          width={"100%"}
+          height={"100vh"}
+        ></Box>
+      </Flex>
+    </Box>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-'use client';
-import { getList } from "./libs/client";
-import { Button  } from '@chakra-ui/react';
+"use client";
+import { Hero } from "./components/Hero";
 
 // TODO: not Foundの場合の処理を追加する
 export default async function Home() {
-
-  return (
-    <div>
-      <Button colorScheme="teal" variant="outline"> 
-        ボタン
-      </Button>
-    </div>
-  );
+  return <Hero />;
 }
