cdc75b8b8d8b78c7473ac2bb58ca4984c68cfa81
kokiito <itou-kouki@aidemy.co.jp>
2023-07-09 12:12:46 +0900

    feat: update about us

--- app/components/organisims/AboutUs.tsx
+++ app/components/organisims/AboutUs.tsx
@@ -15,7 +15,19 @@ export const AboutUs = () => {
         <Center>
           <Heading as={"h1"}>About Us</Heading>
         </Center>
-        <Text></Text>
+        <Box mt={"30px"}>
+          <Text>
+            東京ボルダリングクラブとは都内近郊でボルダリングを一緒に登るために定期的に集うコミュニテイです。
+          </Text>
+        </Box>
+        <Box mt={"20px"}>
+          <Text>おしゃべりするために気軽に参加するもよし</Text>
+          <Text>上達する仲間を探すもよし</Text>
+          <Text>何かイベントを組んで参加者を募るのもよし</Text>
+        </Box>
+        <Box mt={"20px"}>
+          <Text>各々にあったスタイルで参加できます。</Text>
+        </Box>
       </Box>
     </Container>
   );
