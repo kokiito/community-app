ea1fdf1b1b3c3ba0abc6035d0567c341519963fa
kokiito <itou-kouki@aidemy.co.jp>
2023-07-23 11:27:23 +0900

    feat: move favicon files to show favicons for smartphone

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,25 +8,6 @@ export const metadata = {
   title: "Tokyo Bouldering Club",
   description:
     "東京ボルダリングクラブとは都内近郊でボルダリングを一緒に登るために定期的に集うコミュニテイです。",
-  icons: [
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      url: "/favicons/favicon-32x32.png",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "16x16",
-      url: "/favicons/favicon-16x16.png",
-    },
-    {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      url: "/favicons/apple-touch-icon.png",
-    },
-  ],
 };
 
 export default function RootLayout({
@@ -36,26 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="/favicons/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
-        />
-      </Head>
       <body className={inter.className}>
         {" "}
         <Providers>{children}</Providers>
